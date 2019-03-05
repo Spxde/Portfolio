ee79e91ee1faca094c409b09a47a00726e024f62
Spxde <alexis.lottin@ynov.com>
2019-03-05 17:53:01 +0100

    init

---
+++ 07.portfolio/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items: 5,
+  	autoplay: true,
+  	autoplayTimeout: 7000,
+  	autoplaySpeed: 500,
+  	loop: true
+  });
+});
+
+$('.timeline').Timeline();
